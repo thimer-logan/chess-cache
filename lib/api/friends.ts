38893742da8b079d92e1b85692ee5130c4aa7bae
Logan Thimer <thimerlogan@gmail.com>
2025-06-24 06:26:19 -0600

    Added friend and user api CRUD functions

--- lib/api/friends.ts
+++ lib/api/friends.ts
@@ -1,4 +1,4 @@
-import { getSupabase } from "../supabase";
+import { getSupabase } from "@/lib/supabase";
 import { FriendRequest, Profile } from "../types/database.types";
 
 export async function getFriends(): Promise<Profile[]> {
@@ -25,11 +25,74 @@ export async function getFriendRequests(): Promise<FriendRequest[]> {
   return data;
 }
 
+export async function getSentFriendRequests(): Promise<FriendRequest[]> {
+  const supabase = await getSupabase();
+  const { data, error } = await supabase.rpc(
+    "get_pending_sent_friend_requests"
+  );
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
+
 export async function acceptFriendRequest(requestId: string): Promise<boolean> {
   const supabase = await getSupabase();
 
   const { error } = await supabase.rpc("accept_friend_request", {
     p_request_id: requestId,
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return true;
+}
+
+export async function rejectFriendRequest(requestId: string): Promise<boolean> {
+  const supabase = await getSupabase();
+
+  const { error } = await supabase.rpc("decline_friend_request", {
+    p_request_id: requestId,
+  });
+
+  if (error) {
+    console.error("Could not decline request:", error.message);
+    return false;
+  }
+
+  return true;
+}
+
+export async function sendFriendRequest(receiverId: string): Promise<boolean> {
+  const supabase = await getSupabase();
+
+  const { data: user } = await supabase.auth.getUser();
+
+  if (!user || !user.user) {
+    throw new Error("User not found");
+  }
+
+  const { error } = await supabase.from("friend_requests").insert({
+    sender_id: user.user.id,
+    receiver_id: receiverId,
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return true;
+}
+
+export async function removeFriend(friendId: string): Promise<boolean> {
+  const supabase = await getSupabase();
+
+  const { error } = await supabase.rpc("remove_friend", {
+    p_friend_id: friendId,
   });
 
   if (error) {
--- lib/api/user.ts
+++ lib/api/user.ts
@@ -1,8 +1,19 @@
 import { getSupabase } from "../supabase";
 import { Profile } from "../types/database.types";
 
-export async function getUserProfile(user_id: string): Promise<Profile | null> {
+export async function getUserProfile(
+  user_id?: string
+): Promise<Profile | null> {
   const supabase = await getSupabase();
+  if (!user_id) {
+    const { data: user } = await supabase.auth.getUser();
+    user_id = user.user?.id;
+  }
+
+  if (!user_id) {
+    return null;
+  }
+
   const { data, error } = await supabase
     .from("profiles")
     .select("*")
