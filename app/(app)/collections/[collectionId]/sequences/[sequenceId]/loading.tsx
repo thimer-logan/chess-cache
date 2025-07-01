4c12484c5ee5d4023c51666a360f61fbd8dec4f9
Logan Thimer <thimerlogan@gmail.com>
2025-07-01 14:42:07 -0600

    Increased size of loading spinners

--- app/(app)/collections/[collectionId]/sequences/[sequenceId]/loading.tsx
+++ app/(app)/collections/[collectionId]/sequences/[sequenceId]/loading.tsx
@@ -1,5 +1,9 @@
 import { Loader2 } from "lucide-react";
 
 export default function Loading() {
-  return <Loader2 className="animate-spin" />;
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <Loader2 className="animate-spin w-48 h-48" />
+    </div>
+  );
 }
